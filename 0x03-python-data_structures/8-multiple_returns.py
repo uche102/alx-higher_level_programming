d749dfdbe89187cef983a3bc78c1b37088bcfe69
uche102 <nwakoruche192@gmail.com>
2023-03-12 03:08:47 +0100

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to return a tuple with the length of a string
-# and its first character
-#
-# (C) 2022 uche102, Lagos, Nigeria
-# email nwakoruche192@gmail.com
-# -----------------------------------------------------------
-
-
 def multiple_returns(sentence):
-    """
-    Args:
-        sentence: a string argument
-    Returns:
-        a tuple with the length of a string and its first character
-    """
-
-    str_len, first_char = len(sentence), sentence[0]
-    return (str_len, first_char)
-
+    if len(sentence) == 0:
+        my_tuple = (0, None)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
